dbe47fd316e9c516667e18dd7aca2446c270f0fc
fahadbillah <fahadbillah@yahoo.com>
2013-09-01 15:48:16 +0600

    first commit

---
+++ administrator/components/com_dmcfirewall/CHANGELOG.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @Package DMC Firewall
+ * @Copyright Dean Marshall Consultancy Ltd
+ * @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ * Email software@deanmarshall.co.uk
+ * web: http://www.deanmarshall.co.uk/
+ * web: http://www.webdevelopmentconsultancy.com/
+ */
+defined('_JEXEC') or die('Direct access forbidden!');
+?>
+DMC Firewall - 1.1
+================================================================================
+! 'plg_dmcfirewall' reported that every bad attempt was a 'good bad' so wouldn't ban the bad attempts [Core]
+
+DMC Firewall - 1.0
+================================================================================
++ DMC Firewall version 1.0 released!
