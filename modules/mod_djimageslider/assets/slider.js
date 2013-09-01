164df980dd7394f889f0aab0b04648857a47350f
fahadbillah <fahadbillah@yahoo.com>
2013-09-01 15:19:10 +0600

    all eval() deleted except xxx marked files

---
+++ modules/mod_djimageslider/assets/slider.js
@@ -0,0 +1,22 @@
+/**
+
+ * @version $Id: slider_uncompressed.js 11 2013-06-21 11:07:50Z szymon $
+
+ * @package DJ-ImageSlider
+
+ * @subpackage DJ-ImageSlider Component
+
+ * @copyright Copyright (C) 2012 DJ-Extensions.com, All rights reserved.
+
+ * @license DJ-Extensions.com Proprietary Use License
+
+ * @author url: http://dj-extensions.com
+
+ * @author email contact@dj-extensions.com
+
+ * @developer Szymon Woronowski - szymon.woronowski@design-joomla.eu
+
+ *
+
+ */
+
